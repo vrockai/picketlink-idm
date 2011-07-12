18777a3e29fc580a23b9d9b330f208948bb4b6e4
bdaw <boleslaw.dawidowicz@gmail.com>
2011-07-12 20:28:48 +0000

    - fix LDAP matrix testsuite

--- picketlink-idm-testsuite/integration/ldap/src/test/java/org/picketlink/idm/impl/store/ldap/api/OrganizationLDAPTestCase.java
+++ picketlink-idm-testsuite/integration/ldap/src/test/java/org/picketlink/idm/impl/store/ldap/api/OrganizationLDAPTestCase.java
@@ -56,6 +56,8 @@ public class OrganizationLDAPTestCase extends IdentityTestPOJO
       hibernateTest.start();
       ldapTestPOJO.start();
 
+      identityConfig = ldapTestPOJO.getIdentityConfig();
+
       orgTest = new OrganizationTest(this);
 
       ldapTestPOJO.populateClean();
